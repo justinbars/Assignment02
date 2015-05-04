b0ea5e3a72d722857e81d530a87608e9a16e9b04
justinbars <justinbars@hotmail.com>
2015-05-03 19:08:13 -0600

    Initial Commit

---
+++ Assignment02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment02
+//
+//  Created by mac on 5/3/15.
+//  Copyright (c) 2015 Justin Barsketis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
